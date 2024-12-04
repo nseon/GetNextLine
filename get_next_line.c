29aca21a6ff9f371d9c5bce1afffe5137dd5be4d
nseon <nseon@student.42lyon.fr>
2024-12-04 17:39:32 +0100

    starting

--- get_next_line.c
+++ get_next_line.c
@@ -6,9 +6,20 @@
 /*   By: nseon <nseon@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 10:14:35 by nseon             #+#    #+#             */
-/*   Updated: 2024/12/04 13:13:20 by nseon            ###   ########.fr       */
+/*   Updated: 2024/12/04 17:39:17 by nseon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*get_next_line(int fd);
-{}
+{
+	static char	leftover[BUFFER_SIZE];
+	char	*tab;
+	int		i;
+
+	i = 0;
+	read(fd, leftover, BUFFER_SIZE);
+	while (leftover[i] != '\n' && leftover[i])
+	{
+
+	}
+}
